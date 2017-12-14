f161970568c163a14a887ed1432b3f202117733c
PadreCocoRocha <lucasbortoto.lbr@gmail.com>
2017-12-14 14:38:53 -0200

    project start

---
+++ player.h
@@ -0,0 +1,17 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <QWidget>
+
+class player : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit player(QWidget *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // PLAYER_H
