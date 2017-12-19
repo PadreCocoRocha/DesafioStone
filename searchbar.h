deaeed770ba287ce5f42ff886043c96df4c32c03
PadreCocoRocha <lucasbortoto.lbr@gmail.com>
2017-12-19 11:08:29 -0200

    added SearchBar

---
+++ searchbar.h
@@ -0,0 +1,17 @@
+#ifndef SEARCHBAR_H
+#define SEARCHBAR_H
+
+#include <QWidget>
+
+class SearchBar : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit SearchBar(QWidget *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // SEARCHBAR_H
